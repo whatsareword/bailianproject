adba76718934d4b5a096d47236348b0abc60b2e5
whatsareword <806387229@qq.com>
2018-01-18 10:10:35 +0800

    firstsubmit

---
+++ dist/scripts/index.js
@@ -0,0 +1,40 @@
+require(["scripts/config.js"],function(){
+	require(["jquery"/*,"twiceNav","supperBanner","Pop","loadLi","shoppingCar"*/],function($/*,twinav,supb,pop,loadli,shopcar*/){
+		//广告栏
+		// $("#ad-top").find("a").on("click",function(){
+		// 	$("#ad-top").remove();
+		// })
+
+		// twinav.init();
+		// $(".container").supperBanner({
+		// 	src:[
+		// 		"http://img.zcool.cn/community/013d825a56bdbea80120121fb8fed4.jpg@1380w",
+		// 		"http://img.zcool.cn/community/017ac55a55de01a80120121f0d0e19.jpg@1380w",
+		// 		"http://img.zcool.cn/community/0100dd5a56e331a80120121fc3a683.jpg@1380w",
+		// 		"http://img.zcool.cn/community/018c935a56e368a80120121f6072e9.jpg@1380w"
+		// 		],
+		
+		// 	autoplay:true
+
+		// })
+		// //登录框
+		// $("#login").on("click",function(){
+		// 	pop.init();
+		
+		// 	//console.log(pop)
+		// })
+		// //图片加载
+
+		// loadli.init($(".goodslist li"));
+		// //购物车
+		// var $shop_btn = $("<button>加入购物车</button>");
+		// $(".goodslist").find("p").after($shop_btn);
+		// var $shop_btn = $(".goodslist").find("button")
+		// $shop_btn.on("click",function(){
+		// 	shopcar.init($shop_btn);
+		// })
+		
+		
+	})
+
+})
---
+++ src/config.js
@@ -0,0 +1,12 @@
+
+requirejs.config({
+	paths:{
+		"jquery":"scripts/jquery-3.2.1",
+		"supperBanner":"scripts/jquery.supperBanner",
+		"Pop":"scripts/pop",
+		"loadLi":"scripts/loadli",
+		"shoppingCar":"scripts/shoppingcar",
+		"twiceNav":"scripts/twicenav"
+
+	}
+})
